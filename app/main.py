7f8907d9e96f2962b65ad8f9383d0b3a5c7369e0
YongAVA <raksasakul@ava.fund>
2020-12-10 21:35:18 +0700

    set tradesummary

--- app/main.py
+++ app/main.py
@@ -178,8 +178,7 @@ def tradesum_set(start: str='2015-01-01', end: str=datetime.datetime.today().str
     output = crud.get_set_trade_summary(start, end, db)
     if output is None:
         raise HTTPException(status_code=404, detail="Symbol not found")
-    j = output.json()
-    df = pandas.DataFrame(j)
+    df = pandas.DataFrame(json.loads(output))
     df['FundValNetSum']    = round(df['FundValNet'].cumsum(),2)
     df['ForeignValNetSum'] = round(df['ForeignValNet'].cumsum(),2)
     df['TradingValNetSum'] = round(df['TradingValNet'].cumsum(),2)

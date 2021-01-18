33bfae0fe5310a491291773d6a9850b2d92d3b80
YongAVA <raksasakul@ava.fund>
2021-01-18 18:50:53 +0700

    allow cors

--- app/main.py
+++ app/main.py
@@ -22,8 +22,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    # allow_origins=["*"],
-    allow_origin_regex=['*',"http://127.0.0.1:8080"],
+    allow_origins=["*"],
+    # allow_origin_regex=['*',"http://127.0.0.1:8080"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
